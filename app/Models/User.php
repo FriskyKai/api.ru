2641268a5adc62d9ef07781e6c0bd24ec494ebe6
FriskyKai <aptem5196@gmail.com>
2023-12-04 19:19:43 +0700

    Initial commit

---
+++ app/Models/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'surname', 'name', 'patronymic', 'sex', 'birth', 'login', 'password', 'email'
+    ];
+    protected $hidden = [
+        'created_at', 'updated_at'
+    ];
+
+    public function carts() {
+        return $this->hasMany(Cart::class);
+    }
+
+    public function role() {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function orders() {
+        return $this->hasMany(Order::class);
+    }
+}
